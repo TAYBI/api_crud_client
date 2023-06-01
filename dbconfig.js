40d79d40bc1814b52360f900bc68925981d7d51a
Bilal <taybib4@gmail.com>
2023-06-01 17:38:26 +0100

    first commit

---
+++ dbconfig.js
@@ -0,0 +1,26 @@
+const config = {
+    userName: 'sa',
+    password: 'Vpi@maroc@1234',
+    database: 'VPI INFO TEST', 
+    server: 'DESKTOP-KRHHQLB\\SQLEXPRESS',
+    driver: 'msnodesqlv8',
+    options:{
+        trustedConnection : true
+    }
+
+/*    server: 'DESKTOP-KRHHQLB\\SQLEXPRESS',
+    //database: 'VPI INFO TEST',
+    options: {
+        userName: 'sa',
+        password: 'Vpi@maroc@123'
+    },
+    options: {
+        encrypt: true,
+        database: 'VPI INFO TEST'  //update me
+    }
+    options: {
+        trustedConnection: true
+    }*/
+}
+
+module.exports = config; 
